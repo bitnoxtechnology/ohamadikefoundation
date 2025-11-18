43dade38e3078a52e8e22f5f67907f8306750e8d
Tobidavid11 <adesinasurprise2006@gmail.com>
2025-11-18 19:26:42 +0100

    updated main.tsx

--- ohamadikefoundation/src/main.tsx
+++ ohamadikefoundation/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './Styles/global.css'
+import "../src/Styles/global.css"
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
